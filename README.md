778617049a87cc87f545df84a1bff820de4191dc
Ahsan Muhammad Yousuf <ahsankhatri1992@gmail.com>
2018-04-21 03:26:17 +0500

    Initialized repository

---
+++ README.md
@@ -0,0 +1,18 @@
+# Overview
+
+A CLI based word guessing game where you need to guess the word to win the game.
+
+### Why this?
+
+Yet another learning program created under 35 minutes including the help of google and SA
+
+# Usage
+
+```
+python main.py
+```
+
+# Changelog
+
+## 1.0
+ - Initial release.
---
+++ main.py
@@ -0,0 +1,67 @@
+import time
+
+def askForLetter():
+    userInput = raw_input("\n>>> Guess your letter: ").upper()
+
+    if not userInput:
+        return askForLetter()
+
+    return userInput[0]
+
+def printGuessWord(letter):
+    global guessWord, word
+
+    for i,x in enumerate(word):
+        if x==letter:
+            guessWord[i] = letter
+
+    return " ".join(guessWord)
+
+def startTime():
+    global startedTime
+
+    if startedTime == 0:
+        startedTime = time.time()
+
+if __name__=="__main__":
+    word           = list("hangman".upper())
+    guessWord      = ['_'] * len(word)
+    attempts       = startedTime = 0
+    alreadyGuessed = []
+
+    print ">>> Welcome to Hangman! Try to guess the word with minimum attempts."
+    print " ", "_ " * len(word)
+
+    while word != guessWord:
+        letter = askForLetter()
+
+        startTime() # Start timer
+
+        # Already guess intimation
+        try:
+            if letter in alreadyGuessed:
+                print "You have already guessed this letter, please guess another one.\n"
+                continue
+        except Exception:
+            pass
+
+        attempts += 1
+        alreadyGuessed.append(letter)
+
+        # Incorrect intimation
+        try:
+            if letter not in word:
+                print "Incorrect!"
+                continue
+        except Exception:
+            pass
+
+        print " ", printGuessWord(letter)
+
+    diff = int(time.time() - startedTime)
+    minutes, seconds = diff // 60, diff % 60
+    print "\n", "Congratulations! You guess the correct word in", attempts, "attempts and you took " + str(minutes).zfill(2) + ':' + str(seconds).zfill(2), 'seconds'
+
+    # Save user data for future statistics
+    with open('history.txt', 'a') as historyFile:
+        historyFile.write("".join(word)+'|'+str(attempts)+'|'+str(diff)+"\n")
